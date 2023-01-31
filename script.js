adbc007fd7cf78fca3e726dd606114f5b996f17d
Prisca <priscaonyemaechi26@gmail.com>
2023-01-30 23:14:15 -0800

    Mon, 30th Jan

--- script.js
+++ script.js
@@ -172,20 +172,55 @@
 //   }
 // }
 
-function pow(x,n) {
-  let result = x;
+// function pow(x,n) {
+//   let result = x;
 
-  for(let i = 1; i < n; i++) {
-    result *= x;
-  }
-  return result;
-}
+//   for(let i = 1; i < n; i++) {
+//     result *= x;
+//   }
+//   return result;
+// }
 
-let x = prompt('x?', '');
-let n = prompt('n?', '');
+// let x = prompt('x?', '');
+// let n = prompt('n?', '');
 
-if(n < 1){
-  alert('Power ${n} is not supported, use a positive integer');
-} else {
-  alert(pow(x,n));
-}
+// if(n < 1){
+//   alert('Power ${n} is not supported, use a positive integer');
+// } else {
+//   alert(pow(x,n));
+// }
+
+// ARROW FUNCIONS
+// function ask(question, yes, no) {
+//   if(confirm(question)) yes();
+//   else no();
+// }
+
+// ask('Do you agree?',
+// ()=> alert('You agreed.'),
+// ()=> alert('You canceled the execution.')
+// );
+// https://mega.nz/folder/D0cTSaiZ#9DeMyehNmqhBepwKqc_bIQ/folder/ug03CQSb
+
+
+// CODE QUALITY
+// function pow(x ,n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+// let x = prompt("x?", ''); 
+// let n = prompt("n?", '');
+
+// if (n <= 0) {
+//   alert(`Power ${n} is not supported, 
+//     please enter an integer number greater than zero`);
+// } else {
+//   alert( pow(x, n) );
+// }
+

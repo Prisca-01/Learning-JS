e022b761bda7125c3c35d5ddcf9f5e08721c7c09
Prisca <priscaonyemaechi26@gmail.com>
2023-02-02 22:52:23 -0800

    Another day

---
+++ proj.js
@@ -0,0 +1,26 @@
+const count = document.querySelector(".count");
+// const add = document.querySelector(".add");
+// const resetCount = document.querySelector(".reset");
+// const sub = document.querySelector(".subtract");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e) => {
+    if (event.target.classlist.contains("add")) {
+        
+    }
+})
+
+
+
+
+
+
+// add.addEventListener("click", () => {
+//     count.innerHTML++;
+// }) 
+// sub.addEventListener("click", () => {
+//     count.innerHTML--;
+// }) 
+// resetCount.addEventListener("click", () => {
+//     count.innerHTML = 0;
+// }) 

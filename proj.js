352136394f6cc659e6db44f3929c98de4cdeb96c
Prisca <priscaonyemaechi26@gmail.com>
2023-02-07 21:48:42 -0800

    Counter App complete

--- proj.js
+++ proj.js
@@ -1,14 +1,33 @@
-const count = document.querySelector(".count");
 // const add = document.querySelector(".add");
 // const resetCount = document.querySelector(".reset");
 // const sub = document.querySelector(".subtract");
+const count = document.querySelector(".count");
 const buttons = document.querySelector(".buttons");
 
 buttons.addEventListener("click", (e) => {
-    if (event.target.classlist.contains("add")) {
-        
+    if (e.target.classList.contains("add")) {
+        count.innerHTML++;
+        setColor();
     }
-})
+    if (e.target.classList.contains("subtract")) {
+        count.innerHTML--;
+        setColor();
+    }
+    if (e.target.classList.contains("reset")) {
+        count.innerHTML = 0;
+        setColor();
+    }
+});
+
+function setColor() {
+    if (count.innerHTML > 0) {
+        count.style.color = "yellow"
+    } else if (count.innerHTML < 0) {
+        count.style.color = "orangered"
+    } else {
+        count.style.color = "#fff"
+    }
+}
 
 
 

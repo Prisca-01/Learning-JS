57b60e3e03e77ac78ef593bcf5df24fb01b3221c
Prisca <priscaonyemaechi26@gmail.com>
2023-04-01 23:36:26 -0700

    Arrays

--- datatypes.js
+++ datatypes.js
@@ -131,4 +131,83 @@
 function extractCurrencyValue(str) {
     return +str.slice(1);
 }
-alert( extractCurrencyValue('$120') );
+// alert( extractCurrencyValue('$120') );
+
+
+
+// ARRAYS
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  
+//   alert( matrix[2][1] );
+
+//TASKS
+// 1
+let fruits = ["Apples", "Pear", "Orange"];
+
+// push a new value into the "copy"
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+// what's in fruits?
+alert( fruits.length ); // 4
+
+// 2
+// Create an array styles with items “Jazz” and “Blues”.
+// Append “Rock-n-Roll” to the end.
+// Replace the value in the middle with “Classics”. Your code for finding the middle value should work for any arrays with odd length.
+// Strip off the first value of the array and show it.
+// Prepend Rap and Reggae to the array.
+
+// SO;UTION
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+alert( styles.shift() );
+
+styles.unshift("Rap", "Reggae");
+alert(styles);
+
+// 3
+let arr = ["a", "b"];
+
+arr.push(function() {
+  alert( this );
+});
+
+arr[2](); // ?
+
+// 4
+// Write the function sumInput() that:
+
+// Asks the user for values using prompt and stores the values in the array.
+// Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+// Calculates and returns the sum of array items.
+// P.S. A zero 0 is a valid number, please don’t stop the input on zero.
+
+// SOLUTION
+function sumInput() {
+
+    let numbers = [];
+
+    while(true) {
+        let value = prompt("Enter a number", "0");
+
+        if (value === NaN || value === "" || !isFinite(value) ) break;
+
+        numbers.push(+value);
+    }
+
+    let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+alert( sumInput() );
+

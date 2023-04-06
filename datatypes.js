8265a1d9b67d0e5be1a2c2be976a457b2b0d3631
Prisca <priscaonyemaechi26@gmail.com>
2023-04-05 21:16:45 -0700

    ALX tasks day

--- datatypes.js
+++ datatypes.js
@@ -220,3 +220,5 @@ alert( sumInput() );
 // Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
 
 // That is: removes all dashes, each word after dash becomes uppercased.Arr
+
+// PERFORMING MY ALX TASKS TODAY

9c91823b2d3d4faa28a2ca6fe2eb88445988f4ea
Prisca-01 <priscaonyemaechi26@gmail.com>
2024-03-07 09:21:55 +0100

    Learnt how to search the DOM

--- new-project/main.js
+++ new-project/main.js
@@ -1,9 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+import "/bling.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -18,7 +19,118 @@ document.querySelector('#app').innerHTML = `
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Instrument</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th>Name</th>
+          <th>Instrument</th>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr>
+          <td>John Lennon</td>
+          <td>Rhythm Guitar</td>
+        </tr>
+        <tr>
+          <td>Paul McCartney</td>
+          <td>Bass</td>
+        </tr>
+        <tr>
+          <td>George Harrison</td>
+          <td>Lead Guitar</td>
+        </tr>
+        <tr>
+          <td>Ringo Starr</td>
+          <td>Drums</td>
+        </tr>
+      </tbody>
+    </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Instrument</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th>Name</th>
+          <th>Instrument</th>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr>
+          <td>John Lennon</td>
+          <td>Rhythm Guitar</td>
+        </tr>
+        <tr>
+          <td>Paul McCartney</td>
+          <td>Bass</td>
+        </tr>
+        <tr>
+          <td>George Harrison</td>
+          <td>Lead Guitar</td>
+        </tr>
+        <tr>
+          <td>Ringo Starr</td>
+          <td>Drums</td>
+        </tr>
+      </tbody>
+    </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Instrument</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th>Name</th>
+          <th>Instrument</th>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr>
+          <td>John Lennon</td>
+          <td>Rhythm Guitar</td>
+        </tr>
+        <tr>
+          <td>Paul McCartney</td>
+          <td>Bass</td>
+        </tr>
+        <tr>
+          <td>George Harrison</td>
+          <td>Lead Guitar</td>
+        </tr>
+        <tr>
+          <td>Ringo Starr</td>
+          <td>Drums</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>`;
+
+setupCounter($("#counter"));
+
+// console.log(table.children)
+// console.log(row[0].insertCell)
+
+const table = $('table')
+const row = table.rows
+// console.log(row)
+// console.log(row[0])
+
+
+// matches - element.matches('css selector') - returns a boolean
+const counter = $('#counter')
+console.log(counter.matches('button'))
+
+counter.closest('div').style.backgroundColor='red'

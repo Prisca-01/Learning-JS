ce43861be59caac150cb371f4face0a1dd6afc5d
Prisca-01 <priscaonyemaechi26@gmail.com>
2024-04-09 03:37:41 +0100

    Started React

---
+++ full react project/src/App.jsx
@@ -0,0 +1,25 @@
+// // import React  from "react";
+// import confetti from "canvas-confetti"; 
+// import "./App.css";
+
+// function App() {
+
+//   function addConfetti(event){
+//     console.log(event)
+//     confetti({
+//       particleCount: 5,
+//       origin: {
+//         x: event.pageX / window.innerWidth,
+//         y: event.pageY / window.innerHeight,
+//       }
+//     })
+//   }
+//   return (
+//     <div onMouseMove={addConfetti}>
+//       <h1>Hello World</h1>
+//       <p>This is a react App</p>
+//     </div>
+//   )
+// }
+
+// export default App;
